848f0fcce1bfbaf24acb08035fa33a763784a6d0
Gergely Both <11244975-gergely.both@users.noreply.gitlab.com>
2024-02-23 22:22:18 +0100

    further extension

--- music_theory_db.py
+++ music_theory_db.py
@@ -1,3 +1,4 @@
+#TODO: enharmony grouping system: namedtuple, simplify
 import string
 
 valid_names = string.ascii_lowercase[:6]
@@ -6,16 +7,18 @@ valid_symbols = "b#"
 major_steps = [0, 2, 4, 5, 7, 9, 11]
 major_mode_names = ["ionian", "dorian", "phrygian", "lydian", "mixolydian", "aeolian", "locrian"]
 major_mode_starts = {k:v for (k,v) in zip(major_steps, major_mode_names)}
+all_major_scales = {}
+all_major_modes = {}
 
-#TODO: enharmony grouping system fix: namedtuple?
 all_notes_db = {}
-#### unmarked notes add to db (C Major scale)
+# C major scale / white keys only + copy to all scales
 for i in range(len(names_order)):
     all_notes_db[major_steps[i]] = list(names_order[i])
-#### all marked notes add to db (plus enharmonics)
+all_major_scales = all_notes_db.copy()
+# black keys plus enharmonics
 for symbol in valid_symbols:
     symbol_value = -1 if symbol == "b" else +1
-    modifier = (12 + symbol_value) % 12
+    modifier = (12 + symbol_value) % 12 # make into fn or sth
     for name in valid_names:
         for steps in all_notes_db:
             if note_order[steps] == name:
@@ -24,7 +27,6 @@ for symbol in valid_symbols:
                 elif steps + modifier not in all_notes_db:
                     all_notes_db[steps + modifier] = list(None, name + symbol)
 
-#TODO correct # or b display
 circle_of_fifths = []
 while not (circle_of_fifths[0] == circle_of_fifths[-1] and len(circle_of_fifths) > 1):
     if not circle_of_fifths:
@@ -50,10 +52,36 @@ for j in len(circle_of_flats):
     value = circle_of_flats[:i+1][0] or circle_of_flats[:i+1][1]
     flat_major_scales[key] = value
 
-all_major_scales = {}
+for i in sharp_major_scales:
+    key = i
+    value = []
+    for j in all_notes_db:
+        if i in all_notes_db[j]:
+            for k in major_steps:
+                names = all_notes_db[(j+k)%12]
+                for x in sharp_major_scales:
+                    if sharp_major_scales[x] in names:
+                        value.append(sharp_major_scales[x])
+    all_major_scales[key] = value
 
+for l in flat_major_scales:
+    key = l
+    value = []
+    for m in all_notes_db:
+        if l in all_major_modes[m]:
+            for n in major_steps:
+                names = all_notes_db[(m+n)%12]
+                for y in flat_major_scales:
+                    if flat_major_scales[y] in names:
+                        value.append(flat_major_scales[y])
+    all_major_scales[key] = value
 
 all_major_modes = {}
+for n in len(major_mode_names):
+    for z in all_major_scales:
+        scale = all_major_scales[z]
+        new_scale = scale[n:] + scale[:n]
+        name = z + major_mode_names[n]
 
 
 
