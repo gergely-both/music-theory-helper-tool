b27c02394884bb80439d47cea509535191488319
Gergely Both <11244975-gergely.both@users.noreply.gitlab.com>
2024-02-24 22:29:20 +0100

    progressing

--- music_theory_db.py
+++ music_theory_db.py
@@ -32,59 +32,60 @@ for symbol in valid_symbols:
             all_notes_db[new_step] = list(None, new_name) # other solution?
 
 ### CIRCLES OF FIFTH, FOURTH, SHARPS, FLATS
-circle_of_fifths = []
-while not (circle_of_fifths and circle_of_fifths[0] == circle_of_fifths[-1] and len(circle_of_fifths) != 1):
-    if not circle_of_fifths:
-        circle_of_fifths.append(all_notes_db[0]) # alt: less explicitly
+all_fifths = []
+while not (all_fifths and all_fifths[0] == all_fifths[-1] and len(all_fifths) != 1):
+    if not all_fifths:
+        all_fifths.append(all_notes_db[0]) # alt: less explicitly
     else:
-        last_step = find_step(circle_of_fifths[-1])
+        last_step = find_step(all_fifths[-1])
         next_step = last_step + 7 # alt ref: one fifth up
-        circle_of_fifths.append(all_notes_db[next_step])
-circle_of_sharps = circle_of_fifths[6:]
-circle_of_fourths = circle_of_fifths[::-1]
-circle_of_flats = circle_of_fourths[2:9]
+        all_fifths.append(all_notes_db[next_step])
+circle_of_fifths = [(x[0] or x[2]) for x in all_fifths]
+circle_of_sharps = [x[2] for x in all_fifths[6:]]
+all_fourths = all_fifths[::-1]
+circle_of_fourths = [(x[0] or x[1]) for x in all_fourths]
+circle_of_flats = [x[1] for x in all_fourths[2:9]]
 
-### SHARP AND FLAT SCALES, ALL MODES GEN
+### SHARPENED / FLATTENED MAJOR SCALES START / NAME AND SHARP / FLAT MEMBERS
 sharp_major_scales = {}
 for i in range(len(circle_of_sharps)):
-    key = circle_of_fifths[i+1][0] or circle_of_fifths[i+1][2]
-    value = circle_of_sharps[:i][0] or circle_of_sharps[:i][2]
+    key = circle_of_fifths[i+1]
+    value = circle_of_sharps[:i]
     sharp_major_scales[key] = value
 
 flat_major_scales = {}
 for j in range(len(circle_of_flats)):
-    key = circle_of_fourths[i+1][0] or circle_of_fourths[i+1][1]
+    key = circle_of_fourths[i+1]
     key = key if key != "B" else "Cb" # make better
-    value = circle_of_flats[:i][0] or circle_of_flats[:i][1]
+    value = circle_of_flats[:i]
     flat_major_scales[key] = value
 
-for i in sharp_major_scales:
-    key = i
-    value = []
-    base_step = find_step(i)
-    for k in major_steps:
-        many_names = all_notes_db[(j+k)%12]
+def find_correct_name(names, scale_type):
+        for x in scale_type:
+            for y in names:
+                for z in scale_type[x]:
+                    if y == z:
+                        return z
 
-#    for j in all_notes_db:
-#        if i in all_notes_db[j]:
-#            for k in major_steps:
-#                names = all_notes_db[(j+k)%12]
-#                for x in sharp_major_scales:
-#                    if sharp_major_scales[x] in names:
-#                        value.append(sharp_major_scales[x])
-#    all_major_scales[key] = value
+### SHARP AND FLAT SCALES NAMES AND ALL THEIR MEMBERS 
+#TODO merge all dicts somehow?
+for x in sharp_major_scales: # combine both major types (sharp, flat)?
+    key = x
+    values = []
+    base = find_step(key)
+    for step in major_steps:
+        many_names = all_notes_db[(base + step)%12]
+        values.append(find_correct_name(many_names, sharp_major_scales)
+        all_major_scales[key] = values
 
-for l in flat_major_scales:
-    key = l
-    value = []
-    for m in all_notes_db:
-        if l in all_major_modes[m]:
-            for n in major_steps:
-                names = all_notes_db[(m+n)%12]
-                for y in flat_major_scales:
-                    if flat_major_scales[y] in names:
-                        value.append(flat_major_scales[y])
-    all_major_scales[key] = value
+for y in flat_major_scales:
+    key = y
+    values = []
+    base = find_step(key)
+    for step in major_steps:
+        many_names = all_notes_db[(base + step)%12]
+        values.append(find_correct_name(many_names, flat_major_scales)
+        all_major_scales[key] = values
 
 all_major_modes = {}
 for n in range(len(major_mode_names)):
