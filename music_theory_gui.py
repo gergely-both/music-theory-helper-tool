b6b0a43676c1c5382a4bf357abd9b9bd6e040fa8
Gergely Both <11244975-gergely.both@users.noreply.gitlab.com>
2024-03-10 17:39:42 +0100

    some gui fixes

--- music_theory_gui.py
+++ music_theory_gui.py
@@ -1,69 +1,75 @@
 import tkinter as tk
 from music_theory_db import valid_names, valid_symbols, find_note
 
+button_properties = {
+"width": 3,
+"relief": "raised",
+}
 
 class Window:
     def __init__(self, master):
         self.master = master
         self.master.title("Music Theory Helper Tool")
+        # self.master.geometry("400x400")
+        # self.master.resizable(0, 0)
         self.label_guide = tk.Label(master, text="Enter the starting / base note:", height=5)
         self.label_guide.grid(row=0, columnspan=7)
-        self.label = tk.Label(master, height=10)
+        self.label = tk.Label(master, height=5)
         self.label.grid(row=1, column=0, columnspan=7)
-        self.button_c = tk.Button(master, text="C", command=lambda: self.button_input("C"))
+        self.button_c = tk.Button(master, text="C", command=lambda: self.button_input("C"), **button_properties)
         self.button_c.grid(row=2, column=0)
-        self.button_d = tk.Button(master, text="D", command=lambda: self.button_input("D"))
+        self.button_d = tk.Button(master, text="D", command=lambda: self.button_input("D"), **button_properties)
         self.button_d.grid(row=2, column=1)
-        self.button_e = tk.Button(master, text="E", command=lambda: self.button_input("E"))
+        self.button_e = tk.Button(master, text="E", command=lambda: self.button_input("E"), **button_properties)
         self.button_e.grid(row=2, column=2)
-        self.button_f = tk.Button(master, text="F", command=lambda: self.button_input("F"))
+        self.button_f = tk.Button(master, text="F", command=lambda: self.button_input("F"), **button_properties)
         self.button_f.grid(row=2, column=3)
-        self.button_g = tk.Button(master, text="G", command=lambda: self.button_input("G"))
+        self.button_g = tk.Button(master, text="G", command=lambda: self.button_input("G"), **button_properties)
         self.button_g.grid(row=2, column=4)
-        self.button_a = tk.Button(master, text="A", command=lambda: self.button_input("A"))
+        self.button_a = tk.Button(master, text="A", command=lambda: self.button_input("A"), **button_properties)
         self.button_a.grid(row=2, column=5)
-        self.button_b = tk.Button(master, text="B", command=lambda: self.button_input("B"))
+        self.button_b = tk.Button(master, text="B", command=lambda: self.button_input("B"), **button_properties)
         self.button_b.grid(row=2, column=6)
-        self.button_sharp = tk.Button(master, text="#", width=0, command=lambda: self.button_input("#"))
+        self.button_sharp = tk.Button(master, text="#", command=lambda: self.button_input("#"), **button_properties)
         self.button_sharp.grid(row=3, column=4, columnspan=2)
-        self.button_flat = tk.Button(master, text="b", width=0, command=lambda: self.button_input("b"))
+        self.button_flat = tk.Button(master, text="b", command=lambda: self.button_input("b"), **button_properties)
         self.button_flat.grid(row=3, column=1, columnspan=2)
-        self.button_ok = tk.Button(master, text="OK", state="disabled", width=10, command=lambda: self.button_input("OK"))
-        self.button_ok.grid(row=4, column=0, columnspan=7, pady=25)
-        self.button_next = tk.Button(master, text="next", state="disabled", width=10, command=lambda: self.button_input("NEXT"))
-        self.button_next.grid(row=5, column=0, columnspan=7, pady=10)
+        self.button_ok = tk.Button(master, text="OK", state="disabled", command=lambda: self.button_input("OK"), **button_properties)
+        self.button_ok.grid(row=4, column=0, columnspan=7)
+        self.button_next = tk.Button(master, text="next", state="disabled", command=lambda: self.button_input("NEXT"), **button_properties)
+        self.button_next.grid(row=5, column=0, columnspan=7)
 
-        self.input = []
+        self.inputs = []
         self.queue = []
 
     #TODO: queue names to notes correl sys solve trace
     def button_input(self, value):
         if value in valid_names:
-            self.input.clear()
-            self.input.append(value)
+            self.inputs.clear()
+            self.inputs.append(value)
         elif value in valid_symbols:
-            if self.input:
-                if len(self.input) == 1:
-                    self.input.append(value)
-                elif len(self.input) == 2:
-                    if value != self.input[1]:
-                        self.input[1] = value
-                    elif value == self.input[1]:
-                        del self.input[1]
+            if self.inputs:
+                if len(self.inputs) == 1:
+                    self.inputs.append(value)
+                elif len(self.inputs) == 2:
+                    if value != self.inputs[1]:
+                        self.inputs[1] = value
+                    elif value == self.inputs[1]:
+                        del self.inputs[1]
         elif value == "OK":
-            if self.input:
-                new_note = "".join(self.input)
+            if self.inputs:
+                new_note = "".join(self.inputs)
                 if new_note not in self.queue:
                     self.queue.append(new_note)
-                self.input.clear()
+                self.inputs.clear()
         elif value == "NEXT":
             pass
 
 
 
-        if self.input:
+        if self.inputs:
             self.button_ok.config(state="normal")
-        elif not self.input:
+        elif not self.inputs:
             self.button_ok.config(state="disabled")
         if len(self.queue) >= 2:
             self.button_next.config(state="normal")
@@ -77,7 +83,10 @@ class Window:
         if self.queue:
             self.label_guide.config(text="Enter additional notes that belong together:")
             to_show.extend(self.queue)
-        user_input = f'[{"".join(self.input)}]'
+        if self.inputs:
+            user_input = f'[{"".join(self.inputs)}]'
+        elif not self.inputs:
+            user_input = "[ ]"
         to_show.append("".join(user_input))
         self.label.config(text=" ".join(to_show))
 
